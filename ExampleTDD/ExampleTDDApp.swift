3d8968ffa1d7aea0e81edf45fed5ccf72946d945
y4gamilight <ltthanh36711@gmail.com>
2023-08-22 08:30:22 +0700

    -Add source

---
+++ ExampleTDD/ExampleTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTDDApp.swift
+//  ExampleTDD
+//
+//  Created by Le, TanThanh on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
